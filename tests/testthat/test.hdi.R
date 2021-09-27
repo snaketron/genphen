8f7becdf183f253d81cb929a4dcb9322eb9ed680
sktron <simo.kitanovski@uni-due.de>
2021-09-28 00:20:51 +0200

    bump and sync

--- tests/testthat/test.hdi.R
+++ tests/testthat/test.hdi.R
@@ -8,7 +8,11 @@ test_that("getHdi output is numeric vector of length 2", {
   expect_equal(object = length(getHdi(vec = runif(n = 10^3, min = 0, max = 1), 
                                       hdi.level = 0.99)), 
                expected = 2)
-  expect_equal(object = getHdi(vec = runif(n = 10^3, min = 0, max = 1), 
-                               hdi.level = 0.99), 
-               expected = c(0, 1), c(0, 1)+0.05)
+  expect_equal(object = getHdi(vec = rnorm(n = 10^6, mean = 0, sd = 1), 
+                               hdi.level = 0.95)[1], 
+               expected = -1.96, tolerance = 0.1)
+  expect_equal(object = getHdi(vec = rnorm(n = 10^6, mean = 0, sd = 1), 
+                               hdi.level = 0.95)[2], 
+               expected = 1.96, tolerance = 0.1)
 })
+
